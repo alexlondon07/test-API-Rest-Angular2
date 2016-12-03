c9b18a37488ae2378719bc962e1027b73f36250d
Alexander Londoño Espejo <alexander.londono@secuencia24.com>
2016-12-03 12:36:16 -0500

    Armando Barra de Navegación √ Se agrega css bootstrap √ Se agrega font-awesome √

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,22 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template:`
+     <nav class="navbar navbar-default">
+            <div class="container">
+                <div class="navbar-header">
+                    <a class="navbar-brand" href="#">Angular2 Test - Alexander Londoño Espejo</a>
+                </div>
+                <ul class="nav navbar-nav navbar-right">
+                    <li><a href="#"><i class="fa fa-home"></i> Home</a></li>
+                    <li><a href="#"><i class="fa fa-user"></i> API Rest (JSONPlaceholder) - Users</a></li>
+                    <li><a href="#"><i class="fa fa-book"></i> Forms</a></li>
+                </ul>
+            </div>
+            </nav>
+  `,
 })
 export class AppComponent {
-  title = 'app works!';
+
+
 }
